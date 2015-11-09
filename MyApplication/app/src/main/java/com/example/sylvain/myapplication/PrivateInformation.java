718c95fe593bbe7d3e7eb1a2392568c889fec772
Vainsyl <sylvain_rcs@hotmail.com>
2015-11-09 14:33:57 +0100

    vv

---
+++ MyApplication/app/src/main/java/com/example/sylvain/myapplication/PrivateInformation.java
@@ -0,0 +1,13 @@
+package com.example.sylvain.myapplication;
+
+/**
+ * Created by Sylvain on 03/11/15.
+ */
+public class PrivateInformation {
+
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Veuillez saisir votre nom :");
+    String str = sc.nextLine();
+    System.out.println("Vous avez saisi : " + str);
+
+}
